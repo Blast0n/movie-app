b334b574222023e376163c4a86b444b9df355f93
Ilya Pronyakin <blastinboosting@gmail.com>
2024-01-28 16:03:01 +0300

    fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar({ onChangeNav }) {
   const items = [
     {
       key: '1',
-      label: 'Active',
+      label: 'Search',
     },
     {
       key: '2',
@@ -13,6 +13,14 @@ export default function Navbar({ onChangeNav }) {
     },
   ];
   return (
-    <Tabs destroyInactiveTabPane defaultActiveKey="1" items={items} onChange={(key) => onChangeNav(key)} centered />
+    <Tabs
+      destroyInactiveTabPane
+      // eslint-disable-next-line
+      size={'large'}
+      defaultActiveKey="1"
+      items={items}
+      onChange={(key) => onChangeNav(key)}
+      centered
+    />
   );
 }
